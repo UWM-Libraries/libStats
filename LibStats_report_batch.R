817b060c69ae93851b8b093df5afc268cee52f0f
kbriney <kabriney@gmail.com>
2018-07-09 12:23:28 -0500

    Made batch processing more efficient with lists of departments and people and two loops; also changed call to pdf report

--- LibStats_report_batch.R
+++ LibStats_report_batch.R
@@ -6,7 +6,7 @@ library(rmarkdown)
 f_in <- "C:/Users/briney/Documents/JobDocs/LibStats/Reports/data"
 f_out <- "C:/Users/briney/Documents/JobDocs/LibStats/Reports/reports"
 period_Start <- "2018-01-01 00:00:00"
-period_End <- "2018-05-30 11:59:59"
+period_End <- "2018-05-30 23:59:59"
 
 
 ## Edit dates to add to file name
@@ -15,33 +15,22 @@ startDate <- paste(substr(period_Start, 1,4),substr(period_Start, 6,7),substr(pe
 endDate <- paste(substr(period_End, 1,4),substr(period_End, 6,7),substr(period_End, 9,10),sep="")
 
 
-## Create reports
+## Create department reports
 
 fname <- paste(f_out, "/", startDate, "to", endDate, "_Statbot_dept_", sep="")
+deptName <- tibble("AGSL", "Archives", "Curriculum Collection / Music Library", 
+                   "Digital Humanities Lab", "Special Collections", "User Services")
+deptFName <- tibble("AGSL", "Archives", "CurriculumMusic", 
+                    "DHLab", "SpecialCollections", "UserServices")
 
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="AGSL"),
-  output_file = paste(fname,"AGSL.html", sep=""))
+for (i in 1:dim(deptName)) {
+  render("LibStats_report_dept.Rmd", params=list(
+    fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept=deptName[i]),
+    output_file = paste(fname, deptFName[i], ".html", sep=""))
+}
 
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="Archives"),
-  output_file = paste(fname,"Archives.html", sep=""))
 
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="Curriculum Collection / Music Library"),
-  output_file = paste(fname, "CurriculumMusic.html", sep=""))
-
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="Digital Humanities Lab"),
-  output_file = paste(fname, "DHLab.html", sep=""))
-
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="Special Collections"),
-  output_file = paste(fname, "SpecialCollections.html", sep=""))
-
-render("LibStats_report_dept.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, dept="User Services"),
-  output_file = paste(fname, "UserServices.html", sep=""))
+# Create RHD and Welcome Desk reports
 
 render("LibStats_report_RHD.Rmd", params=list(
   fpath=f_in, periodStart=period_Start, periodEnd=period_End),
@@ -51,48 +40,15 @@ render("LibStats_report_welcome.Rmd", params=list(
   fpath=f_in, periodStart=period_Start, periodEnd=period_End),
   output_file = paste(fname, "Welcome.html", sep=""))
 
+
+## Create personal reports
+
 fname <- paste(f_out, "/", startDate, "to", endDate, "_Statbot_person_", sep="")
+persName <- tibble("ganski", "nbungert", "kristinw", "lkopecky", "kbowes", "kabina", 
+                   "wadecg", "mathiasm", "thornto4", "briney", "skorolev")
 
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="ganski"),
-  output_file = paste(fname, "ganski.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="nbungert"),
-  output_file = paste(fname, "nbungert.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="kristinw"),
-  output_file = paste(fname, "kristinw.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="lkopecky"),
-  output_file = paste(fname, "lkopecky.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="kbowes"),
-  output_file = paste(fname, "kbowes.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="kabina"),
-  output_file = paste(fname, "kabina.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="wadecg"),
-  output_file = paste(fname, "wadecg.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="mathiasm"),
-  output_file = paste(fname, "mathiasm.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="thornto4"),
-  output_file = paste(fname, "thornto4.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="briney"),
-  output_file = paste(fname, "briney.html", sep=""))
-
-render("LibStats_report_person.Rmd", params=list(
-  fpath=f_in, periodStart=period_Start, periodEnd=period_End, person="skorolev"),
-  output_file = paste(fname, "skorolev.html", sep=""))
+for (i in 1:dim(persName)) {
+  render("LibStats_report_person.Rmd", params=list(
+    fpath=f_in, periodStart=period_Start, periodEnd=period_End, person=persName[i]),
+    output_file = paste(fname, persName[i], ".html", sep=""))
+  }
